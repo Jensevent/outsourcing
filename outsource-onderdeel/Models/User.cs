27bbdf744f9cbb8918d25e479d8f13e0918ad7e8
Graaf,Stefan S.J.C. de <stefan.degraaf@student.fontys.nl>
2020-04-14 16:38:38 +0200

    Update of the game

---
+++ outsource-onderdeel/Models/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace outsource_onderdeel.Models
+{
+    public class User
+    {
+        public int Id { get; private set; }
+        public int LocationId { get; private set; }
+        public string FirstName {get; private set; }
+        public string LastName { get; private set; }
+        public string Email { get; private set; }
+        public string Profile { get; private set; }
+        public string UserFunction { get; private set; }
+        public bool Findable { get; private set; }
+
+        public User(int givenId, int givenLocationId, string givenfn, string givenln, string givenEmail, string givenProfile, string givenuf, bool givenFindable)
+        {
+            this.Id = givenId;
+            this.LocationId = givenLocationId;
+            this.FirstName = givenfn;
+            this.LastName = givenln;
+            this.Email = givenEmail;
+            this.Profile = givenProfile;
+            this.UserFunction = givenuf;
+            this.Findable = givenFindable;
+        }
+        public void SetLocation(int givenId)
+        {
+            this.LocationId = givenId;
+            // update in db
+        }
+
+        public void ShowLocation()
+        {
+
+        }
+    }
+}
